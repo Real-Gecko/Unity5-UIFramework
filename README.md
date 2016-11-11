3e499ff76f320218ac38737c11f4c0d2b73379d4
Real-Gecko <alexandrbezenkov@gmail.com>
2016-11-12 02:07:34 +0600

    Version 0.1

---
+++ README.md
@@ -0,0 +1,1 @@
+﻿UI Framework created to make UI styling and layouting easier for Unity 5
---
+++ UIFramework/Core.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This file is subject to the included LICENSE.md file. 
+ */
+
+using System;
+using UnityEngine;
+
+namespace UIFramework
+{
+	[KSPAddon(KSPAddon.Startup.Instantly,true)]
+	public class Core: MonoBehaviour
+	{
+		static private Core _instance = null;
+		static public Core Instance
+		{
+			get { return _instance; }
+		}
+
+		private bool skinInitialized = false;
+
+		public void Awake() {
+			if (_instance != null) {
+				Destroy (this);
+				return;
+			}
+			_instance = this;
+		}
+
+//		public void Start() {
+//			DontDestroyOnLoad (this);
+//		}
+
+		public void OnDestroy() {
+			if (_instance == this)
+				_instance = null;
+		}
+
+		public void OnGUI() {
+			if (skinInitialized)
+				return;
+			Palette.InitPalette ();
+			Styles.InitStyles ();
+			skinInitialized = true;
+			Destroy (this); // Quit after initialized
+		}
+	}
+}
+
